79da4e14899a18a7631ddd0b8e26841804e1dee0
Gvan-Iovnov <60030514+Gvan-Iovnov@users.noreply.github.com>
2020-06-13 13:00:52 +0300

    installed prettier and eslint + quick fix

--- script.js
+++ script.js
@@ -1,46 +1,52 @@
-window.onload = function(){
-  fistOperrandTextField = document.createElement("input");
-  fistOperrandTextField.type = 'text';
-  fistOperrandTextField.size="40";
-  secondOperrandTextField = document.createElement("input");
-  secondOperrandTextField.type = 'text';
-  secondOperrandTextField.size="40";
-  breakRow = document.createElement('br');
-  plusText = document.createElement("span");
-  plusText.innerHTML = '<p> + </p>';
-  resultButton = document.createElement('input'); 
-  resultButton.id = 'butt';
-  resultButton.type = 'button';
-  resultButton.size = '40';
-  resultButton.value = 'Посчитать';
+window.onload = function main() {
+  const fistOperrandTextField = document.createElement("input");
+  fistOperrandTextField.type = "text";
+  fistOperrandTextField.size = "40";
+  const secondOperrandTextField = document.createElement("input");
+  secondOperrandTextField.type = "text";
+  secondOperrandTextField.size = "40";
+  const breakRow = document.createElement("br");
+  const plusText = document.createElement("span");
+  plusText.innerHTML = "<p> + </p>";
+  const resultButton = document.createElement("input");
+  resultButton.id = "butt";
+  resultButton.type = "button";
+  resultButton.size = "40";
+  resultButton.value = "Посчитать";
 
-  resultButton.onclick = function(){
-    if(document.getElementById("errorMessageOfFirstOperand")!=null)document.getElementById("errorMessageOfFirstOperand").remove();
-    if(document.getElementById("errorMessageOfSecondOperand")!=null)document.getElementById("errorMessageOfSecondOperand").remove();
-    if(document.getElementById("resultText")!=null)document.getElementById("resultText").remove();
+  resultButton.onclick = function getResult() {
+    if (document.getElementById("errorMessageOfFirstOperand") != null)
+      document.getElementById("errorMessageOfFirstOperand").remove();
+    if (document.getElementById("errorMessageOfSecondOperand") != null)
+      document.getElementById("errorMessageOfSecondOperand").remove();
+    if (document.getElementById("resultText") != null)
+      document.getElementById("resultText").remove();
 
-    let valueOfFirstInput = fistOperrandTextField.value.trim();
-    let valueOfSecondInput = secondOperrandTextField.value.trim();
-    let operand1 = parseInt(valueOfFirstInput, 10);
-    let operand2 = parseInt(valueOfSecondInput, 10);
-    let firstInputCheck = isNaN(operand1) || operand1.toString().length !== valueOfFirstInput.length;
-    let secondInputCheck = isNaN(operand2) || operand2.toString().length !== valueOfSecondInput.length
+    const valueOfFirstInput = fistOperrandTextField.value.trim();
+    const valueOfSecondInput = secondOperrandTextField.value.trim();
+    const operand1 = parseInt(valueOfFirstInput, 10);
+    const operand2 = parseInt(valueOfSecondInput, 10);
 
-    if(firstInputCheck){
-      let errorMessage = document.createElement("div");
-      errorMessage.id= "errorMessageOfFirstOperand";
+    const firstInputCheck =
+      Number.isNaN(operand1) || operand1 != valueOfFirstInput;
+    const secondInputCheck =
+      Number.isNaN(operand2) || operand2 != valueOfSecondInput;
+
+    if (firstInputCheck) {
+      const errorMessage = document.createElement("div");
+      errorMessage.id = "errorMessageOfFirstOperand";
       errorMessage.innerHTML = "<p>Это не число</p>";
       fistOperrandTextField.after(errorMessage);
     }
-    if(secondInputCheck){
-      let errorMessage = document.createElement("div");
-      errorMessage.id= "errorMessageOfSecondOperand";
+    if (secondInputCheck) {
+      const errorMessage = document.createElement("div");
+      errorMessage.id = "errorMessageOfSecondOperand";
       errorMessage.innerHTML = "<p>Это не число</p>";
       secondOperrandTextField.after(errorMessage);
     }
-    if(secondInputCheck + firstInputCheck == 0){
-      let result = document.createElement("div");
-      result.id= "resultText";
+    if (secondInputCheck + firstInputCheck === 0) {
+      const result = document.createElement("div");
+      result.id = "resultText";
       result.innerHTML = operand1 + operand2;
       resultButton.after(result);
     }
@@ -51,4 +57,4 @@ window.onload = function(){
   document.body.appendChild(secondOperrandTextField);
   document.body.appendChild(breakRow);
   document.body.appendChild(resultButton);
-}
+};
